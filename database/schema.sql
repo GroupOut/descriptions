85abbdf53bfcf22fae62c053035dec01edfb58d2
leerssej <jens.leerssen@gmail.com>
2018-07-20 08:15:58 -0700

    adds create db and tables script

--- database/schema.sql
+++ database/schema.sql
@@ -7,7 +7,8 @@ USE descriptions;
 
 CREATE TABLE merchants (
   id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  name VARCHAR(120)
+  name VARCHAR(120),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE locations (
@@ -19,31 +20,35 @@ CREATE TABLE locations (
   zip CHAR(2),
   lat DECIMAL(10,7),
   lon DECIMAL(10,7),
-  gp_id VARCHAR(255)
+  gp_id VARCHAR(255),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE categories (
   id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  name VARCHAR(120)
+  name VARCHAR(120),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE deal_cat_joins (
   id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  deal_id INTEGER XYZ,
-  cat_id INTEGER XYZ
+  deal_id INTEGER REFERENCES deals(id),
+  cat_id INTEGER REFERENCES categories(id),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE deals (
   id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   name VARCHAR(255),
-  merch_id INTEGER XYZ,
-  loc_id INTEGER XYZ,
+  merch_id INTEGER REFERENCES (id),
+  loc_id INTEGER REFERENCES (id),
   descrip TEXT,
   addl_info TEXT,
   inclusions TEXT,
   exclusions TEXT,
   fine_print TEXT,
-  deal_cat_id INTEGER XYZ
+  deal_cat_id INTEGER REFERENCES deal_cat_joins(deal_id),
+  PRIMARY KEY (id)
 );
 
 INSERT INTO merchants
