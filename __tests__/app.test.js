e38340adf739e4783457020d6c993ab673d33fd2
leerssej <jens.leerssen@gmail.com>
2018-07-25 09:31:55 -0700

    brings back testing work from https://egghead.io/lessons/react-course-overview-testing-react-components-with-enzyme-and-jest (which didn't work in the tutorial but it's the best foundation I have so far)

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,32 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../public/app';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+// console.log(App);
+
+configure({ adapter: new Adapter() });
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<Description />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+describe('<App />', () => {
+  it('should render App', () => {
+    const wrapper = shallow(<App />)
+    console.log(wrapper.debug())
+  })
+})
