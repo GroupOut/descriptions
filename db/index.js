89750eab0e9ba735b6593061a573bb4538d913f7
leerssej <jens.leerssen@gmail.com>
2018-07-24 21:11:20 -0700

    sets environment testing to node and comments out db password for CirclCI testing

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Af234rew',
+  // password: 'Af234rew',
   database: 'descriptions',
 });
 
