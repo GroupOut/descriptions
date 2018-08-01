1fb0f5386b1edba140fdba5b57059d7cd10a4c02
leerssej <jens.leerssen@gmail.com>
2018-08-01 14:14:14 -0700

    reducing test count to test testing

--- __tests__/description_FrontEndComponent.test.js
+++ __tests__/description_FrontEndComponent.test.js
@@ -1,11 +1,11 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import Description from '../client/components/description';
-// import { configure, shallow, mount } from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
-// import toJson from "enzyme-to-json";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Description from '../client/components/description';
+import { configure, shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import toJson from "enzyme-to-json";
 
-// configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
 
 // describe('the Description Component', () => {
 //   it('should render Description', () => {
@@ -44,29 +44,29 @@
 //   })
 // })
 
-// describe('the Description Lifecycle & Methods', () => {
-//   it('calls componentDidMount', () => {
-//     jest.spyOn(Description.prototype, 'componentDidMount')
-//     const wrapper = shallow(<Description />)
-//     expect(Description.prototype.componentDidMount.mock.calls.length).toBe(1)
-//     wrapper.unmount()
-//   })
+describe('the Description Lifecycle & Methods', () => {
+  it('calls componentDidMount', () => {
+    jest.spyOn(Description.prototype, 'componentDidMount')
+    const wrapper = shallow(<Description />)
+    expect(Description.prototype.componentDidMount.mock.calls.length).toBe(1)
+    wrapper.unmount()
+  })
 
-//   it('calls getDescripInfo in ComponentDidMount', (done) => {
-//     const wrapper = shallow(<Description />)
-//     wrapper.instance().getDescripInfo = jest.fn()
-//     wrapper.instance().componentDidMount()
-//     done()
-//     expect(wrapper.instance().getDescripInfo).toBeCalled()
-//   })
+  it('calls getDescripInfo in ComponentDidMount', (done) => {
+    const wrapper = shallow(<Description />)
+    wrapper.instance().getDescripInfo = jest.fn()
+    wrapper.instance().componentDidMount()
+    done()
+    expect(wrapper.instance().getDescripInfo).toBeCalled()
+  })
 
-//   // it('calls handleSubmit on click', (done) => {
-//   //   const wrapper = shallow(<Description />)
-//   //   const submitButton = wrapper.find('.submitButton')
-//   //   wrapper.instance().handleSubmit = jest.fn()
-//   //   submitButton.simulate('click')
-//   //   done()
-//   //   expect(wrapper.instance().handleSubmit).toBeCalled()
-//   // })
+  // it('calls handleSubmit on click', (done) => {
+  //   const wrapper = shallow(<Description />)
+  //   const submitButton = wrapper.find('.submitButton')
+  //   wrapper.instance().handleSubmit = jest.fn()
+  //   submitButton.simulate('click')
+  //   done()
+  //   expect(wrapper.instance().handleSubmit).toBeCalled()
+  // })
 
-// })
+})
