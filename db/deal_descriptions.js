afcea65bdd1b75e5c6be817741b788e63f389397
leerssej <jens.leerssen@gmail.com>
2018-08-01 20:47:44 -0700

    just the descrip table

--- db/deal_descriptions.js
+++ db/deal_descriptions.js
@@ -5,15 +5,15 @@ const connection = require(path.join(__dirname, 'index.js'));
 const getDescriptionData = (dealId, callback) => {
   connection.query(
     `
-      SELECT 
-        d.id deal_id,
-        d.name deal_name, 
+      SELECT * from whatyoullget;
+      --  d.id deal_id,
+      --  d.name deal_name, 
       --  m.name merch_name, 
-        ds.descrip_p1 descrip, 
-        d.addl_info,
-        d.inclusions, 
-        d.exclusions, 
-        d.fine_print
+      --  ds.descrip_p1 descrip, 
+      --  d.addl_info,
+      --  d.inclusions, 
+      --  d.exclusions, 
+      --  d.fine_print
         -- GROUP_CONCAT (c.name SEPARATOR ', ') ttd,
         -- l.addr_ln1, 
         -- l.addr_ln2, 
@@ -23,15 +23,15 @@ const getDescriptionData = (dealId, callback) => {
         -- l.lon,
         -- l.lat,
         -- l.gp_id
-      FROM deal d
-      INNER JOIN whatyoullget ds ON ( ds.deal_id = d.id  )
+      -- FROM deal d
+      -- INNER JOIN whatyoullget ds ON ( ds.deal_id = d.id  )
       -- INNER JOIN merchant m ON ( d.merch_id = m.id  )
       -- INNER JOIN location l ON ( d.loc_id = l.id  )
       -- INNER JOIN deal_cat_join dcj ON ( d.id = dcj.deal_id  )  
       -- INNER JOIN category c ON ( dcj.cat_id = c.id  )
-      WHERE d.id = ${dealId}
-      GROUP BY d.id
-      ORDER BY d.id
+      -- WHERE d.id = ${dealId}
+      -- GROUP BY d.id
+      -- ORDER BY d.id
   ;`,
     (error, result, fields) => {
       if (error) {
