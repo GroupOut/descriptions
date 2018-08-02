1fae36dbc9620b271c23b9cc225ed9ee4ef4a650
leerssej <jens.leerssen@gmail.com>
2018-08-01 17:06:30 -0700

    adds slice of description table

--- db/deal_descriptions.js
+++ db/deal_descriptions.js
@@ -9,7 +9,7 @@ const getDescriptionData = (dealId, callback) => {
         d.id deal_id,
         d.name deal_name, 
         m.name merch_name, 
-        -- d.descrip, 
+        ds.descrip_p1, 
         d.addl_info,
         d.inclusions, 
         d.exclusions, 
@@ -24,6 +24,7 @@ const getDescriptionData = (dealId, callback) => {
         l.lat,
         l.gp_id
       FROM deal d
+      INNER JOIN description ds ON ( ds.deal_id = d.id  )
       INNER JOIN merchant m ON ( d.merch_id = m.id  )
       INNER JOIN location l ON ( d.loc_id = l.id  )
       INNER JOIN deal_cat_join dcj ON ( d.id = dcj.deal_id  )  
